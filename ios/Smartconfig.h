bb9dfa55c663dd0c4524d868c45abf378ece5379
Truong Hoang Hieu <hieumath85@gmail.com>
2021-03-29 00:51:19 +1100

    add ios library

--- ios/Smartconfig.h
+++ ios/Smartconfig.h
@@ -1,5 +1,13 @@
+//#import <React/RCTBridgeModule.h>
+#import <Foundation/Foundation.h>
 #import <React/RCTBridgeModule.h>
+#import <React/RCTLog.h>
+#import <React/RCTUtils.h>
 
+#import "ESPTouchTask.h"
+#import "ESPTouchResult.h"
+#import "ESP_NetUtil.h"
+#import "ESPTouchDelegate.h"
 @interface Smartconfig : NSObject <RCTBridgeModule>
 
 @end
